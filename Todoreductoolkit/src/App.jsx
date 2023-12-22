eda7eeca195b34981dd4a5852472403bcc825f94
Neeraj <neerajjoshi9389798147@gmail.com>
2023-12-22 21:19:32 +0530

    project done

---
+++ Todoreductoolkit/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AddTodo from './Components/addtodo'
+import Todos from './Components/Todos'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <h1>Redux tool kit</h1>
+   <AddTodo/>
+   <Todos/>
+    </>
+  )
+}
+
+export default App
